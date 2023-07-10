15b0e1591f8089171263abb6bfade13f5c4868a5
Harut <har8mail@gmail.com>
2023-07-10 21:13:51 +0400

    adding store routing.

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,4 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('form');
-});
+Route::post('/form', [FormController::class, 'store'])->name('form.store');
